519a9567d58815c3b2f265e1690ef381baf7c767
uldisn <uldisn@09516a42-71f9-43be-811f-9b821cf0d246>
2009-11-28 16:43:51 +0000

    fixed table cogsglpostings name

--- branches/utf-8/sql/mysql/upgrade3.12-3.13.sql
+++ branches/utf-8/sql/mysql/upgrade3.12-3.13.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `www_users` ADD COLUMN `pdflanguage` tinyint(1) NOT NULL DEFAULT '0';
-ALTER TABLE `cogsglposting` CHANGE `area` CHAR(3) NOT NULL DEFAULT '';
+ALTER TABLE `cogsglpostings` CHANGE `area` CHAR(3) NOT NULL DEFAULT '';
