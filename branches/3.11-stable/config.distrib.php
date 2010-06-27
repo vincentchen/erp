4fb14c63864a43c14ddd3b3c92f65e526cc6fe39
tim_schofield <tim_schofield@09516a42-71f9-43be-811f-9b821cf0d246>
2010-06-27 21:30:39 +0000

    New version number

--- branches/3.11-stable/config.distrib.php
+++ branches/3.11-stable/config.distrib.php
@@ -12,7 +12,7 @@ $allow_demo_mode = True;
 
 // webERP version
 
-$Version = '3.11.1';
+$Version = '3.11.3';
 
 // The timezone of the business - this allows the possibility of having
 // the web-server on a overseas machine but record local time
