e09fa64f2308f22990d56b8871c42566ee25c31a
ExsonQu <ExsonQu@09516a42-71f9-43be-811f-9b821cf0d246>
2012-03-25 13:56:02 +0000

    25/3/2012 Exson: Add return back link for SupplierInvoice.php while users make input errors. Reported by Ke from webERP Chinese Community QQ group

--- branches/stable/SupplierInvoice.php
+++ branches/stable/SupplierInvoice.php
@@ -1504,6 +1504,9 @@ then do the updates and inserts to process the invoice entered */
 
 } /*end of process invoice */
 
+if($InputError==true){ //add a link to return if users make input errors.
+	echo '<div class="centre"><a href="'.$rootpath.'/SupplierInvoice.php" >'._('Back to Invoice Entry').'</a></div>';
+}
 echo '</form>';
 include('includes/footer.inc');
 ?>
