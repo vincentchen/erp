05fb4a3b02d9e16b5246d014f99ecee7c846953c
ExsonQu <ExsonQu@09516a42-71f9-43be-811f-9b821cf0d246>
2012-05-08 11:05:13 +0000

    8/05/2012: MTPubRadio fixed the lastcostupdate failure to add new Items by alter stockmaster table.

--- branches/stable/sql/mysql/upgrade4.07-4.08.sql
+++ branches/stable/sql/mysql/upgrade4.07-4.08.sql
@@ -1,1 +1,3 @@
-UPDATE config SET confvalue='4.07.8' WHERE confname='VersionNumber';
+ALTER TABLE `stockmaster` CHANGE `lastcostupdate` `lastcostupdate` DATE NOT NULL DEFAULT '0000-00-00';
+ALTER TABLE paymentmethods ADD opencashdrawer tinyint NOT NULL default '0';
+UPDATE config SET confvalue='4.07.9' WHERE confname='VersionNumber';
