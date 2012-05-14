291a2d5f6cc760cb0da6618da2a2ed3bba535740
ExsonQu <ExsonQu@09516a42-71f9-43be-811f-9b821cf0d246>
2012-05-14 02:53:59 +0000

    14/5/2012 Exson: Prepare for new release.

--- branches/stable/sql/mysql/upgrade4.07-4.08.sql
+++ branches/stable/sql/mysql/upgrade4.07-4.08.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `stockmaster` CHANGE `lastcostupdate` `lastcostupdate` DATE NOT NULL DEFAULT '0000-00-00';
+ALTER TABLE `stockmaster` CHANGE `lastcostupdate` `lastcostupdate` DATE;
 ALTER TABLE paymentmethods ADD opencashdrawer tinyint NOT NULL default '0';
 UPDATE config SET confvalue='4.07.9' WHERE confname='VersionNumber';
